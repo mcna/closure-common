a939970f4b72d3370b0a8345ab0c647ba7a119f3
David Lichteblau <david@lichteblau.com>
2008-11-30 14:58:56 +0100

    release

--- dist.sh
+++ dist.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/sh -e
+set -x
+
 cd $(dirname $0)
 home=$(pwd)
 name=$(basename $home)
-dir=${name}-$(date --iso)
+name_and_date=${name}-$(date --iso)
 
 TMPDIR=`mktemp -d /tmp/dist.XXXXXXXXXX`
 cleanup() {
@@ -11,11 +13,36 @@ cleanup() {
 }
 trap cleanup exit
 
-cd $TMPDIR
-cvs -d "`cat $home/CVS/Root`" export -r HEAD -d "$dir" "$name"
+# make -C doc
 
-tgz=$TMPDIR/${dir}.tgz
-tar czf $tgz $dir
+git tag -f $name_and_date
+git archive --prefix=$name_and_date/ $name_and_date | \
+    ( cd $TMPDIR && tar xvf - )
+
+# echo '(progn (load "dist.lisp") (quit))' | clbuild lisp 
+
+# rsync -a doc $TMPDIR/$name_and_date
+
+cd $TMPDIR
+
+tgz=$TMPDIR/${name_and_date}.tgz
+tar czf $tgz $name_and_date
 gpg -b -a $tgz
 
-mv $tgz $tgz.asc $home/
+mkdir -p ~/clnet/project/cxml/public_html/
+
+#rsync -av \
+#    $name_and_date/doc/ \
+#    ~/clnet/project/cxml/public_html/
+
+rsync $tgz $tgz.asc ~/clnet/project/cxml/public_html/download/
+
+rm -f ~/clnet/project/cxml/public_html/download/closure-common.tar.gz 
+rm -f ~/clnet/project/cxml/public_html/download/closure-common.tar.gz.asc
+
+ln -sf ${name_and_date}.tgz ~/clnet/project/cxml/public_html/download/closure-common.tar.gz
+ln -sf ${name_and_date}.tgz.asc ~/clnet/project/cxml/public_html/download/closure-common.tar.gz.asc
+
+echo done
+exit 0
+rsync -av ~/clnet/project dlichteblau@common-lisp.net:/
